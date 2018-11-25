33802659a7f105622486ebda8ba5c29d39f5e0d7
walkinsky <lyh6188@hotmail.com>
2018-11-25 17:09:49 +0800

    improved main

--- nana/runner/sample/nana_runner.cpp
+++ nana/runner/sample/nana_runner.cpp
@@ -6,21 +6,34 @@
 #include <nana/runner/sample/editor_app.h>
 #include <nana/runner/sample/demo_app.h>
 
+int run_main(runa::pcwstr _cmdline)
+{
+    runa::sample::editor_app a;
+    //runa::sample::demo_app a;
+    a.run(_cmdline);
+    return 0;
+}
+
+int run_main_no_exception(runa::pcwstr _cmdline)
+{
+    try {
+        return run_main(_cmdline);
+    }
+    catch (std::exception& e) {
+        NAR_LOG_EXCPT(e);
+    }
+    return -1;
+}
+
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     _In_opt_ HINSTANCE hPrevInstance,
     _In_ LPWSTR    lpCmdLine,
     _In_ int       nCmdShow)
 {
-    using namespace runa;
-    //try {
-        sample::editor_app a;
-        //sample::demo_app a;
-        a.run(lpCmdLine);
-    //}
-    //catch (std::exception& e) {
-    //    NAR_LOG_EXCPT(e);
-    //    return -1;
-    //}
-    return 0;
+#ifdef _DEBUG
+    return run_main(lpCmdLine);
+#else
+    return run_main_no_exception(lpCmdLine);
+#endif
 }
 
